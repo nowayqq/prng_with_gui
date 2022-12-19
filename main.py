4995f322341c930abac438b8092e4a4c7fabfb88
nowayqq <set.mr@inbox.ru>
2022-12-19 12:33:48 +1000

    Added the ability to select the method and data of the library random

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import PySimpleGUI as sg
 import numpy as np
 import matplotlib.pyplot as plt
+import random as rnd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from middleSquares import get_values_ms
 from pylab import rcParams
+from middleSquares import get_values_ms
+from middleMult import get_values_mm
+from mixing import get_values_mix
+from lcm import get_values_lcm
+
 
 rcParams['figure.figsize'] = 16, 6.5
 
-# VARS CONSTS:
-# Upgraded dataSize to global...
 _VARS = {'window': False,
          'fig_agg': False,
          'pltFig': False,
          'dataSize': 100,
          'seed': None,
-         'max_value': 100}
+         'max_value': 100,
+         'method': 0}
+
+_METHODS = ['Middle squares', 'Middle multiplications',
+            'Mixing', 'Linear congruent']
 
 plt.style.use('Solarize_Light2')
 
@@ -38,7 +45,14 @@ sg.theme('black')
 # New layout with slider and padding
 
 layout = [[sg.Canvas(key='figCanvas', background_color='#FDF6E3')],
-          [sg.Text(text="Random sample size :",
+          [sg.Combo(_METHODS, size=(20, 0),
+                    enable_events=True, key='-LIST-',
+                    default_value=_METHODS[0],
+                    background_color='Grey',
+                    text_color='Black',
+                    pad=((36, 0), (10, 0)),
+                    readonly=True),
+           sg.Text(text="Random sample size: ",
                    font=SliderFont,
                    background_color='#FDF6E3',
                    pad=((0, 0), (10, 0)),
@@ -51,18 +65,18 @@ layout = [[sg.Canvas(key='figCanvas', background_color='#FDF6E3')],
                      enable_events=True),
            sg.Button('Resample',
                      font=AppFont,
-                     pad=((4, 0), (0, 0)), size=(10, 0))],
+                     pad=((4, 200), (0, 0)), size=(10, 0))],
           # pad ((left, right), (top, bottom))
-          [sg.Input(key='-IN-', pad=((197, 0), (10, 0)), size=(24, 0), text_color='Black', background_color='Grey'),
-           sg.Button('Set seed', font=AppFont, pad=((8, 0), (0, 0)), size=(10, 0)),
-           sg.Button('Set MaxValue', font=AppFont, pad=((8, 0), (0, 0)), size=(10, 0))],
-          [sg.Button('Exit', font=AppFont, pad=((512, 0), (3, 0)), size=(10, 0))]]
+          [sg.Input(key='-IN-', pad=((197, 0), (10, 0)), size=(25, 0), text_color='Black', background_color='Grey'),
+           sg.Button('Set seed', font=AppFont, pad=((0, 9), (0, 0)), size=(10, 0)),
+           sg.Button('Set MaxValue', font=AppFont, pad=((0, 8), (0, 0)), size=(10, 0))],
+          [sg.Button('Exit', font=AppFont, pad=((504, 0), (3, 0)), size=(10, 0))]]
 
-_VARS['window'] = sg.Window('Random Samples',
+_VARS['window'] = sg.Window('PRNG',
                             layout,
                             finalize=True,
                             resizable=True,
-                            location=(100, 100),
+                            location=(0, 0),
                             element_justification="center",
                             background_color='#FDF6E3')
 
@@ -72,9 +86,24 @@ _VARS['window'] = sg.Window('Random Samples',
 
 # \\  -------- PYPLOT -------- //
 
+def chooseMethod(val):
+
+    if val == 0:
+        return get_values_ms(seed=_VARS['seed'], size=_VARS['dataSize'], maxValue=_VARS['max_value'])
+
+    if val == 1:
+        return get_values_mm(seed=_VARS['seed'], size=_VARS['dataSize'], maxValue=_VARS['max_value'])
+
+    if val == 2:
+        return get_values_mix(seed=_VARS['seed'], size=_VARS['dataSize'], maxValue=_VARS['max_value'])
+
+    if val == 3:
+        return get_values_lcm(seed=_VARS['seed'], size=_VARS['dataSize'], maxValue=_VARS['max_value'])
+
 
 def makeSynthData():
-    yData = get_values_ms(seed=_VARS['seed'], size=_VARS['dataSize'], maxValue=_VARS['max_value'])
+
+    yData = chooseMethod(_VARS['method'])
     xData = np.linspace(0, _VARS['dataSize'],
                         num=_VARS['dataSize'], dtype=int)
 
@@ -85,6 +114,11 @@ def drawChart():
     _VARS['pltFig'] = plt.figure()
     dataXY = makeSynthData()
     plt.plot(dataXY[0], dataXY[1], '.k')
+    rnd.seed(_VARS['seed'])
+    if (_VARS['max_value'] == 1):
+        plt.plot(dataXY[0], [rnd.random() for i in range(_VARS['dataSize'])], '.k', color='orange')
+    else:
+        plt.plot(dataXY[0], [rnd.randrange(_VARS['max_value']) for i in range(_VARS['dataSize'])], '.k', color='orange')
     _VARS['fig_agg'] = draw_figure(
         _VARS['window']['figCanvas'].TKCanvas, _VARS['pltFig'])
 
@@ -95,6 +129,11 @@ def updateChart():
     # plt.cla()
     plt.clf()
     plt.plot(dataXY[0], dataXY[1], '.k')
+    rnd.seed(_VARS['seed'])
+    if (_VARS['max_value'] == 1):
+        plt.plot(dataXY[0], [rnd.random() for i in range(_VARS['dataSize'])], '.k', color='orange')
+    else:
+        plt.plot(dataXY[0], [rnd.randrange(_VARS['max_value']) for i in range(_VARS['dataSize'])], '.k', color='orange')
     _VARS['fig_agg'] = draw_figure(
         _VARS['window']['figCanvas'].TKCanvas, _VARS['pltFig'])
 
@@ -122,6 +161,21 @@ def updateSeed(val):
         updateChart()
 
 
+def updateMethod(val):
+    if val == _METHODS[0]:
+        _VARS['method'] = 0
+        updateChart()
+    elif val == _METHODS[1]:
+        _VARS['method'] = 1
+        updateChart()
+    elif val == _METHODS[2]:
+        _VARS['method'] = 2
+        updateChart()
+    elif val == _METHODS[3]:
+        _VARS['method'] = 3
+        updateChart()
+
+
 drawChart()
 
 
@@ -137,5 +191,7 @@ while True:
         updateSeed(values['-IN-'])
     elif event == 'Set MaxValue':
         updateMaxValue(values['-IN-'])
+    elif event == '-LIST-':
+        updateMethod(values['-LIST-'])
 
 _VARS['window'].close()
